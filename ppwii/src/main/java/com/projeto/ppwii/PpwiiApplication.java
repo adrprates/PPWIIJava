d6bc837dadfb061fc9afcef0d363b6a8676c1cb2
Adriano Prates <adrianopratessouza@gmail.com>
2025-03-29 14:15:16 -0300

    Primeiro commit

---
+++ ppwii/src/main/java/com/projeto/ppwii/PpwiiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ppwii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpwiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpwiiApplication.class, args);
+	}
+
+}
---
+++ ppwii/src/test/java/com/projeto/ppwii/PpwiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ppwii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpwiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
